f1bfe5f44acdfa94c45e70ae6081a17c79eae985
yizanchen <31357578+yizanchen@users.noreply.github.com>
2019-10-19 17:54:59 -1000

    work in p

--- js/otherlink.js
+++ js/otherlink.js
@@ -1,4 +1,6 @@
-var numlist = [0,0,0,0,0,0,0,0,0,0]
+var numlist = [0,0,0,0,0,0,0,0,0,0];
+var money = 3;
+var operation = '+';
 
 function makeNum(){
     var value = Math.ceil(Math.random() * 6)
@@ -17,5 +19,31 @@ function getnum(){
     document.getElementById('numlist').innerText = numlist.join('  -  ');    
 }
 
+function changeOp(flag){
+    if(flag==1){
+        operation = '+';
+    }else operation = '-';
+    document.getElementById('operation').innerText=operation;
+}
+
+function doCal(val){
+    if(operation == '+'){
+        money += val;
+    }else money -= val;
+    document.getElementById('money').innerText=money;
+}
+function clearM(){
+    money = 0;
+    document.getElementById('money').innerText=money;
+
+}
+function DoMTen(){
+    money -=10;
+    document.getElementById('money').innerText=money;
+}
+
 getnum();
 makeNum();
+
+document.getElementById('money').innerText=money;
+document.getElementById('operation').innerText=operation;
