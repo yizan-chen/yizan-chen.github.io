fb64145fc5dc6565b5d9a516240739692d6d8f78
yizanchen <31357578+yizanchen@users.noreply.github.com>
2020-06-28 22:57:52 -1000

    project section update

--- js/custom.js
+++ js/custom.js
@@ -2,29 +2,23 @@ function moveTo(eid){
     window.scrollTo(0,eid.offsetTop-60);
 }
 
+// function showTime() {
+//     var d = new Date();
+//     var e = d.toLocaleTimeString();
+//     document.getElementById("time").innerHTML = e;
+//     setTimeout(function(){ showTime() }, 500);
+// }
 
-function showTime() {
-    var d = new Date();
-    var e = d.toLocaleTimeString();
-    document.getElementById("time").innerHTML = e;
-    setTimeout(function(){ showTime() }, 500);
+window.onscroll = function changeNav(){
+    if( window.pageYOffset > 50){
+        document.getElementById("nav").classList.add("scrolled");
+    }else{
+        document.getElementById("nav").classList.remove("scrolled");
+    }
 }
 
-function greet(time){
-    var grt = "Hello, how are the stars tonight? ";
-    if (time > 21 )
-        grt = "Hello, how are the stars tonight?";
-    else if(time > 16)
-        grt = "Good Evening";    
-    else if(time > 11)
-        grt = "Good Afternoon";
-    else if(time > 4)
-        grt = "Good Morning";
-    document.getElementById("greeting").innerHTML = grt;
-}
 
 var de = new Date();
-greet(de.getHours());
-showTime();
+//showTime();
 
 console.log("Working");
